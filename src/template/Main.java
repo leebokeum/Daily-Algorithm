0912db23b02da0d23f79e5f9fade2c63c895cbb5
leebokeum <leebokeum87@gmail.com>
2019-12-30 19:53:23 +0900

    Initial commit

---
+++ src/template/Main.java
@@ -0,0 +1,13 @@
+package template;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * 백준온라인 자바 코드 제출 템플릿
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+    }
+}
